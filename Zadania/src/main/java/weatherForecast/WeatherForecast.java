766280edfcb0a8f23ff5659d075294f4aff50d7a
Krzysztof Kaczyński <qris70@gmail.com>
2018-05-26 09:28:40 +0200

    Wzorce projektowe Zadanie weatherForecast implementuje interfejs Observable

--- Zadania/src/main/java/weatherForecast/WeatherForecast.java
+++ Zadania/src/main/java/weatherForecast/WeatherForecast.java
@@ -1,4 +1,25 @@
 package weatherForecast;
 
-public class WeatherForecast {
+import notification.Observer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WeatherForecast implements Observable {
+
+    private int temperature;
+    private int pressure;
+    private Set<Observer> registerObserver = new HashSet<Observer>();
+
+    public void registerObserver(Observer observer) {
+
+    }
+
+    public void unregisterObserver(Observer observer) {
+
+    }
+
+    public void notifyObserver(Observer observer) {
+
+    }
 }
