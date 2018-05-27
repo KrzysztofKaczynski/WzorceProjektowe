267c5c8df1d7e1090cb5f8c5b8099a84d410a4ab
Krzysztof Kaczyński <qris70@gmail.com>
2018-05-27 08:46:50 +0200

    Wzorce projektowe Strategia Zadanie Restauracja add Main new Chef "Magda Dupssler"

--- Zadania/Strategia/src/main/java/restauracja/Main.java
+++ Zadania/Strategia/src/main/java/restauracja/Main.java
@@ -1,4 +1,12 @@
 package restauracja;
 
+import restauracja.chef.Chef;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Nowe zamówienie - jajka na twardo!
+        Chef chef = new Chef("Magda Dupssler");
+        chef.cook();
+    }
 }
