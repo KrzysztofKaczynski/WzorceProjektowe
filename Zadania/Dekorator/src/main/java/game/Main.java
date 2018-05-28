82183afe17365035f617d2b81f2b01bdd439a1ce
Krzysztof Kaczyński <qris70@gmail.com>
2018-05-28 14:10:24 +0200

    Wzorce projektowe add Dekorator "GRA" - implementacja.

--- Zadania/Dekorator/src/main/java/game/Main.java
+++ Zadania/Dekorator/src/main/java/game/Main.java
@@ -10,8 +10,10 @@ public class Main {
         Terrain terrain = new Plain();
         Terrain terrain1 = new Hill();
         Terrain terrain2 = new Swamp(new Hill());
-//        Terrain terrain3 = new SwampForestPlain();
-
-        System.out.println("Swamp hill cost" + terrain2);
+        Terrain terrain3 = new Swamp(new ForestDecorator(new Plain()));
+        Terrain terrain4 = new RoadDecorator(new Hill());
+        System.out.println("Swamp hill cost" + terrain2.fuelCost());
+        System.out.println("Swap forest plain cost" + terrain3.fuelCost());
+        System.out.println(terrain4.getDescription() + "" + terrain3.fuelCost());
     }
 }
