c47093e8a99efae1d0d7db72332d32654aceaff9
Krzysztof Kaczyński <qris70@gmail.com>
2018-05-31 21:07:08 +0200

    add HouseDirector

---
+++ Zadania/BuilderHouseinterface/src/main/java/house_builder/SmallHouseBuilder.java
@@ -0,0 +1,42 @@
+package house_builder;
+
+public class SmallHouseBuilder implements HouseBuilder {
+
+    private House house;
+
+    public SmallHouseBuilder(){
+        this.house = new House();
+    }
+
+    public void buildWalls() {
+
+    }
+
+    public void buildFloors() {
+
+    }
+
+    public void buildRooms() {
+
+    }
+
+    public void buildRoof() {
+
+    }
+
+    public void buildWindows() {
+
+    }
+
+    public void buildDoors() {
+
+    }
+
+    public void buildGarage() {
+
+    }
+
+    public House getHouse() {
+        return null;
+    }
+}
