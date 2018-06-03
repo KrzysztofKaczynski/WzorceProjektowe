46141b9dccffceb1274063be4e567c85d7207c3c
Krzysztof Kaczyński <qris70@gmail.com>
2018-06-03 08:12:45 +0200

    Clean and add names

---
+++ Zadania/BuilderFlighLeg/src/main/java/fligh_leg/Main.java
@@ -0,0 +1,9 @@
+package fligh_leg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").build();
+        System.out.println(leg);
+    }
+}
--- Zadania/BuilderHouseinterface/src/main/java/Main.java
+++ Zadania/BuilderHouseinterface/src/main/java/Main.java
@@ -1,2 +1,24 @@
+import house_builder.BigHouseBuilder;
+import house_builder.House;
+import house_builder.HouseDirector;
+import house_builder.SmallHouseBuilder;
+
 public class Main {
+
+    public static void main(String[] args) {
+        SmallHouseBuilder smallHouseBuilder = new SmallHouseBuilder();
+        BigHouseBuilder bigHouseBuilder = new BigHouseBuilder();
+
+        HouseDirector smallhouseDirector = new HouseDirector(smallHouseBuilder);
+        smallhouseDirector.buildHouse();
+
+        HouseDirector bighouseDirector = new HouseDirector(bigHouseBuilder);
+        bighouseDirector.buildHouse();
+
+        House smallHouse = smallhouseDirector.getHouse();
+        House bigHouse = bighouseDirector.getHouse();
+
+        System.out.println(smallHouse);
+        System.out.println(bigHouse);
+    }
 }
