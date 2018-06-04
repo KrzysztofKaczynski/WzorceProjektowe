b993903461b9b038ed8f4e5dad4536fbf4775aa2
Krzysztof Kaczyński <qris70@gmail.com>
2018-06-04 11:24:38 +0200

    Wzorce projektowe add SingletonGameEngine plus enum GameEngineEnum.

--- Zadania/SingletonGameEngine/src/main/java/Main.java
+++ Zadania/SingletonGameEngine/src/main/java/Main.java
@@ -1,2 +1,17 @@
+import singleton_game.GameEngine;
+
+import java.io.Serializable;
+
 public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        GameEngine engine = GameEngine.getInstance();
+        GameEngine engine2 = GameEngine.getInstance();
+
+        System.out.println(engine==engine2);
+    }
+
 }
--- Zadania/SingletonGameEngine/src/main/java/singleton_game/GameEngine.java
+++ Zadania/SingletonGameEngine/src/main/java/singleton_game/GameEngine.java
@@ -1,4 +1,32 @@
 package singleton_game;
 
-public class GameEngine {
+import java.io.Serializable;
+
+public class GameEngine implements Serializable {
+    private static final long serialVersionUID = 23232323;
+
+    private int hp = 100;
+    private String characterName = "";
+
+    private static GameEngine instance = new GameEngine();
+
+    private GameEngine(){
+
+    }
+    public void run(){
+        while (true){
+            //czekamy na input gracza
+            //zmieniamy stan gry
+            //renderujemy grafike
+        }
+
+    }
+
+    public static GameEngine getInstance(){
+
+        return instance;
+    }
+    protected Object readResolve(){
+        return getInstance();
+    }
 }
